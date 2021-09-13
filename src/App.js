1e305f9513196841976c25f4476a82e0ade5f0ac
marcos <marc0sdurval20@gmail.com>
2021-09-13 19:27:06 -0300

    inicio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './redux';
 
 function App() {
+  const [car, setValue] = useState({
+    yellow:false,
+    red: false,
+    outro: false,
+  })
+  function handleSetValue ({id}) {
+    setValue((prev) => ({...car,[id]: !prev[id]}))
+  }
+  const objApp = {car,handleSetValue}
+
   return (
-    <Cars />
+    <MyContext.Provider value={objApp}>
+      <Cars />
+    </MyContext.Provider>
   );
 }
 
--- src/Cars.jsx
+++ src/Cars.jsx
@@ -1,17 +1,83 @@
 // src/Cars.jsx
 
-import PropTypes from 'prop-types';
 import React from 'react';
-import { connect } from 'react-redux';
 import carBlue from './images/carBlue.jpeg';
 import carRed from './images/carRed.jpeg';
 import carYellow from './images/carYellow.jpeg';
-import { moveCar } from './redux/actionCreators';
+import MyContext from './redux/index'
 
-function Cars({ redCar, blueCar, yellowCar, moveCar }) {
+function Cars() {
   return (
-    <div>
+    <MyContext.Consumer >
+      {
+        ({car:{yellow,outro,red},handleSetValue}) => {
+          // console.log(yellow)
+          return(
+            <>
+            <div>
+            <img
+              className={yellow ? 'car-right' : 'car-left'}
+              src={carYellow}
+              alt="yellow car"
+            />
+            <button
+              id="yellow"
+              onClick={(e) => handleSetValue(e.target)}
+              type="button"
+            >
+              Move
+            </button>
+          </div>
+          <div>
+        <img
+          className={outro ? 'car-right' : 'car-left'}
+          src={carBlue}
+          alt="blue car"
+        />
+        <button
+          id="outro"
+          onClick={(e) => handleSetValue(e.target)}
+          type="button"
+        >
+          Move
+        </button>
+      </div>
       <div>
+      <img
+          className={red ? 'car-right' : 'car-left'}
+          src={carRed}
+          alt="red car"
+        />
+        <button
+          type="button"
+          id="red"
+          onClick={(e) => handleSetValue(e.target)}
+        >
+          Move
+        </button>
+      </div>
+          </>
+          )
+        }
+      }
+    </MyContext.Consumer>
+  );
+}
+
+// Cars.propTypes = {
+//   moveCar: PropTypes.func.isRequired,
+//   blueCar: PropTypes.bool.isRequired,
+//   redCar: PropTypes.bool.isRequired,
+//   yellowCar: PropTypes.bool.isRequired,
+// };
+
+// const mapStateToProps = (state) => ({
+//   redCar: state.cars.red,
+//   blueCar: state.cars.blue,
+//   yellowCar: state.cars.yellow});
+
+// const mapDispatchToProps = { moveCar };
+      /* <div>
         <img
           className={redCar ? 'car-right' : 'car-left'}
           src={carRed}
@@ -49,23 +115,6 @@ function Cars({ redCar, blueCar, yellowCar, moveCar }) {
         >
           Move
         </button>
-      </div>
-    </div>
-  );
-}
+      </div> */
 
-Cars.propTypes = {
-  moveCar: PropTypes.func.isRequired,
-  blueCar: PropTypes.bool.isRequired,
-  redCar: PropTypes.bool.isRequired,
-  yellowCar: PropTypes.bool.isRequired,
-};
-
-const mapStateToProps = (state) => ({
-  redCar: state.cars.red,
-  blueCar: state.cars.blue,
-  yellowCar: state.cars.yellow});
-
-const mapDispatchToProps = { moveCar };
-
-export default connect(mapStateToProps, mapDispatchToProps)(Cars);
+export default Cars;
